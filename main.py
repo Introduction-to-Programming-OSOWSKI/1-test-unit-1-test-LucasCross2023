6fe68c25bcb136c9f812acf8993af521a05e8ab6
LucasCross2023 <817750@mystma.org>
2020-12-22 16:57:24 +0000

    test try 1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#WRITE YOUR CODE IN THIS FILE
+def name(n):
+    return n
+
+print (name("Lucas"))
+
+def favorites(favCol, favNum):
+    return "Your favorite color is " + favCol + " and your favorite number is " + favNum
+
+print(favorites("orange", "19"))
+
+def bigAdd(x, y, z, a, b):
+    return x + y + z + a + b
+
+print(bigAdd( 2, 5, 1, 9, 5))
+
+def fourOperations(x, y, z, a, b):
+    return x + y - z * a - b
+
+print(fourOperations( 2, 5, 1, 9, 5))
